f2edc224f5de59b53f2eb271c9f293e291295392
Peter_YSQH <72824407+AprilSweettooth@users.noreply.github.com>
2023-08-07 17:07:24 +0100

    implementing & comparing term grouping with openf

--- qDrift/hamsimqDrift copy.py
+++ qDrift/hamsimqDrift copy.py
@@ -19,45 +19,49 @@ class AlgorithmHamSimqDrift:
     """
     def __init__(self,
                  initial_state: Circuit, 
-                 qubit_operator: list,
+                 qubit_operator: "list[QubitPauliOperator]",
                  coeff: list,
+                #  measurements: "list[QubitPauliOperator]",
+                 dict_qubit_operator: QubitPauliOperator,
                  t_max: float,
                  reps: int,
+                 symbol: str,
                  *args,
                  **kwargs):
 
         self.circuit = initial_state
-    
+        self.c_copy = self.circuit.copy()
         self._n_qubits = initial_state.n_qubits
         self._initial_state = AerStateBackend().run_circuit(initial_state).get_state()
         self._qubit_operator = qubit_operator
-   
+        t=fresh_symbol("t")
+        self.H = dict_qubit_operator
+        self.H.subs({symbol:1})
         self.coeff = coeff
         self.t_max = t_max
         self._rep = reps
         self._time_step = t_max/self._rep
         self._time_space = np.linspace(0,t_max,self._rep+1)
-
+        # self.m = measurements
+        # f = lambda m: m[0] if len(m) == 1 else m[0] + f(m[1:])
+        # self._measurements_overall = f(measurements)
+        # self._measurements = [m.to_sparse_matrix(self._n_qubits) for m in measurements]
         self.backend = AerBackend()
         self.statebackend = AerStateBackend()
-        self.U_sims = []
-        # self.U_sim = np.zeros((2**self._n_qubits,2**self._n_qubits)) 
+        self.gate_count = []
+        self.exp = []  
+        self.E = {}
+        self.U_sim = np.zeros((2**self._n_qubits,2**self._n_qubits)) 
 
-    def Drift_step(self):
-        weights = []
-        for i in range(len(self.coeff)):
-            if isinstance(self.coeff[i],list):
-                weights.append(np.mean([self.coeff[i]]))
-            else:
-                raise Exception("Sorry, please give as lists of list")
-        abs_weights = [abs(w) for w in weights]
-        lambd = sum(abs_weights)
-        prob = [weight / lambd for weight in abs_weights]
+    def Drift_step(self, t):
+        weights = [abs(co) for co in self.coeff]
+        lambd = sum(weights)
+        prob = [weight / lambd for weight in weights]
         op_index = list(range(0,len(weights)))
         sample_index = np.random.choice(op_index, self._rep, p=prob)
         sample_ops = [self._qubit_operator[s] for s in sample_index]
         new_coeff = lambd * (self.t_max/(np.pi/2)) / self._rep
-        new_coeffs = [new_coeff*co/abs(co) for co in weights]
+        new_coeffs = [new_coeff*co/abs(co) for co in self.coeff]
         return sample_ops,new_coeffs,sample_index
     
     def qps_from_openfermion(self,paulis):
@@ -92,29 +96,27 @@ class AlgorithmHamSimqDrift:
         return ops
 
     def Drift_exp(self):
-        for n in range(0,self._rep+1):
+        for n in range(self._rep,self._rep+1):
             if n ==0:
                 circ = self.circuit.copy()
             else:
                 circ = self.circuit.copy() 
-                V, coeff, idx = self.Drift_step()
+                V, coeff, idx = self.Drift_step(self._time_space[n])
                 # print(V, coeff, idx)
                 for j in range(n):
-                    for paulis in V[j]:
-                        p = self.Convert_String_to_Op(paulis)
-                        pbox = PauliExpBox(p, coeff[idx[j]])
-                        circ.add_pauliexpbox(pbox, np.arange(self._n_qubits))
+                    V_j = self.Convert_String_to_Op(V[j])
+                    pbox = PauliExpBox(V_j, coeff[idx[j]])
+                    circ.add_pauliexpbox(pbox, np.arange(self._n_qubits))
 
             naive_circuit = circ.copy()
             Transform.DecomposeBoxes().apply(naive_circuit)
             # print(tk_to_qiskit(naive_circuit))
             # self.gate_count.append(naive_circuit.n_gates_of_type(OpType.CX))
-            self.U_sims.append(naive_circuit.get_unitary())
+            self.U_sim = naive_circuit.get_unitary()
             # compiled_circuit = self.statebackend.get_compiled_circuit(naive_circuit)
             # statevec = self.statebackend.run_circuit(compiled_circuit).get_state()
             # self.exp.append(abs(np.vdot(self._initial_state,statevec))**2)
             # self.E[self._time_space[n]] = self.H.state_expectation(statevec, [Qubit(i) for i in range(self._n_qubits)]) 
-        return self.U_sims
 
 
     def execute(self, real=None, color='purple', plot=False):
